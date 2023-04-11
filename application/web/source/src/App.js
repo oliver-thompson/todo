9a0b8c61da15bf4ed1e0af81a08a82b7c7c82e71
Thompson <olthompson@teksystems.com>
2023-04-11 10:33:38 -0500

    app: added react-router-dom dependency and created landing component with routing

--- application/web/source/src/App.js
+++ application/web/source/src/App.js
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Landing from "./views/Landing/Landing";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
+  );
 }
 
 export default App;
